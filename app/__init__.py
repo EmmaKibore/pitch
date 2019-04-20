4b4a756dd85aa9b525b83c9ad4ed37a939a4a9cf
EmmaKibore <emmakibore@gmail.com>
2019-04-20 15:30:20 +0300

    defined bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,6 @@
-from flask import Flask
-from .config import DevConfig
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
-# Initializing application
-app = Flask(__name__,instance_relative_config = True)
-
-#setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
-
-from app import views
+# Initializing flask extensions
+bootstrap.init_app(app)
+db.init_app(app)
