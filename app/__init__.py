24de9c097e5ce5731e41834bd90fe8e0b4d4a90a
EmmaKibore <emmakibore@gmail.com>
2019-04-21 11:02:48 +0300

    initialized flask extensions

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,23 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from .auth import auth as auth_blueprint
+from flask import login manager()
 app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
-# Initializing flask extensions
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
 def create_app(config_name):
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://emmah:kHAOYAISDEAD2008@localhost/test'
     db = SQLAlchemy(app)
 
-    app.config.from_object(config_options[config_name])
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
 
-    bootstrap = Bootstrap(app)
+
+
+    return app
--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from ..import db
 
 #views
 @app.route('/pitch/<int:pitch_id>')
