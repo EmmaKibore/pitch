d7481c44bf08f16dc20cc6f1bfd382665a294fd5
EmmaKibore <emmakibore@gmail.com>
2019-04-23 06:42:48 +0300

    input the configure options

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,14 @@
-from flask import render_template,redirect,url_for,abort
+from flask import Flask
+from flask import render_template,redirect,url_for,abort,flash
 from app import app
 from ..import db,photos
 from flask_login import login_required
-from .. models import User,Reviews
+from .. models import User, Pitch, Comment
 from . forms import RegistrationForm
-from . forms import ReviewForm,UpadteProfile
+from . forms import UpdateProfile, PitchForm, CommentForm
 from ..email import mail_message
-import markdown2
+from . import main
+
 
 
 
@@ -18,22 +20,35 @@ def index(id):
     '''
 
     title = 'Pitch panel'
+    pitches = Pitch.query.all()
 
 
-   return render_template('index.html', title= title, pitchs = pitchs)
+   return render_template('index.html', title= title, pitches = pitches)
 
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
 
-@auth.route('/register',methods = ["GET","POST"])
-def register():
-    form = RegistrationForm()
+    return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
     if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        user.bio = form.bio.data
+
         db.session.add(user)
         db.session.commit()
-        return redirect(url_for('auth.login'))
-        title = "New Account"
-    return render_template('auth/register.html',registration_form = form)
 
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
 @main.route('/user/<uname>/update/pic',methods= ['POST'])
 @login_required
 def update_pic(uname):
@@ -45,28 +60,26 @@ def update_pic(uname):
         db.session.commit()
     return redirect(url_for('main.profile',uname=uname))
 
-@main.route('/user/<uname>')
-def profile(uname):
-    user = User.query.filter_by(username = uname).first()
-
-    if user is None:
-        abort(404)
-
-     form = UpdateProfile()
+@main.route('/pitch/new', methods=['GET','POST'])
+@login_required
+def new_pitch():
+    form = PitchForm()
 
     if form.validate_on_submit():
-        user.bio = form.bio.data
 
-        db.session.add(user)
+        title=form.title.data
+        content=form.content.data
+        category=form.category.data
+        pitch = Pitch(title=title, content=content,category=category)
+        # pitch.save_pitch(pitch)
+        db.session.add(pitch)
         db.session.commit()
 
-        return redirect(url_for('.profile',uname=user.username))
+        flash('Your pitch has been created!', 'success')
+        return redirect(url_for('main.index', id=pitch.id))
 
+    return render_template('new_pitch.html', title='New Post', pitch_form=form, post ='New Post')
 
-@main.route('/pitch/review/new/<int:id>', methods = ['GET','POST'])
-@login_required
-def new_review(id)
-    return render_template('pitch.html', title = title)
 
 @main.route('/comment/new/<int:id>', methods=['GET','POST'])
 @login_required
@@ -85,4 +98,6 @@ def new_comment(id):
 
     comment = Comment.query.filter_by(pitch_id=id).all()
 
-        return render_template('new_comment.html', title='New Post', comment=comment,comment_form=form, post ='New Post')
+
+
+    return render_template('new_comment.html', title='New Post', comment=comment,comment_form=form, post ='New Post')
--- config.py
+++ config.py
@@ -1,27 +1,24 @@
 import os
 
 class Config:
-    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
-    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://username:password@localhost/Pitch'
-
-
-
-class Config:
-    '''
-    General configuration parent class
-    '''
-    PITCH_API_BASE_URL = 'https://api.thepitchdb.org/3/pitch/{}?api_key={}'
-
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
+
 class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://james:password@localhost/watchlist_test'    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://emmah:password@localhost/pitch_test'
 
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://emmah:Ilovememore100@localhost/pitchpanel'
 
     DEBUG = True
 
